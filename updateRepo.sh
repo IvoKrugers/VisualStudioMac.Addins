8f80b16d319cfdcd1d1c164d459a11ab934fe251
Ivo Krugers <i.krugers@xablu.com>
2022-04-05 22:27:03 +0200

    Add GetGitUrl

--- updateRepo.sh
+++ updateRepo.sh
@@ -11,6 +11,9 @@ pwd
 
 rm *.mpack
 mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ../VisualStudioMac.OneClickToOpenFile/VisualStudioMac.OneClickToOpenFile/bin/VisualStudioMac.OneClickToOpenFile.dll
+mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ../VisualStudioMac.GetGitUrl/VisualStudioMac.GetGitUrl/bin/Release/net471/VisualStudioMac.GetGitUrl.dll 
+
+
 
 /Applications/Visual\ Studio.app/Contents/MacOS/vstool setup rep-build .
 rm index.html
