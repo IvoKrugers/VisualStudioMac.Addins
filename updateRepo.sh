27bb0809ff98c19c5eb4bc03a0b40453b3b4e757
IvoKrugers <ivo.krugers@gmail.com>
2022-04-05 21:16:22 +0200

    initial

---
+++ updateRepo.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SCRIPTFILE=$0
+
+#Get the absolute path to the containing folder
+PROJECTFOLDER=${SCRIPTFILE%/*}
+
+cd ${PROJECTFOLDER}
+
+pwd
+
+rm *.mpack
+mono /Applications/Visual\ Studio.app/Contents/Resources/lib/monodevelop/bin/vstool.exe setup pack ../VisualStudioMac.OneClickToOpenFile/VisualStudioMac.OneClickToOpenFile/bin/VisualStudioMac.OneClickToOpenFile.dll
+
+/Applications/Visual\ Studio.app/Contents/MacOS/vstool setup rep-build .
+rm index.html
+
+#xml ed -i "/Repository" -t text -n "Name" -v "Visual Studio Mac Addins by Ivo Krugers" main.mrep
